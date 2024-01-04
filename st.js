5ef82643bd2699e5dfcf5343b621de18c7cde193
AnushaPulkaram <153076760+AnushaPulkaram@users.noreply.github.com>
2024-01-04 14:59:56 +0530

    Add files via upload

---
+++ st.js
@@ -0,0 +1,4 @@
+function changeimg(){
+    document.getElementById("img1").src="https://th.bing.com/th/id/R.5b58bcbf3a01d5735e3acd728a899ef7?rik=8vS923baXjjW9w&riu=http%3a%2f%2fimages5.fanpop.com%2fimage%2fphotos%2f25500000%2fHarry-Potter-Wallpaper-harry-james-potter-25502412-1024-768.jpg&ehk=usCORLcvpCOsyW4EqaLEFNJAKRJPM%2frH8Zg11KPla%2bo%3d&risl=&pid=ImgRaw&r=0";
+    document.getElementById("img2").src="https://th.bing.com/th/id/OIP.SVjymIT8SrmrtbVjudysMgAAAA?rs=1&pid=ImgDetMain";
+}
